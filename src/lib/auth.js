9f1aeff91e781d326d8139a2c3f309c36df6ba1b
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-05-19 20:49:22 +0600

    better-auth set up done with plugin

---
+++ src/lib/auth.js
@@ -0,0 +1,27 @@
+import { betterAuth } from "better-auth";
+import { jwt } from "better-auth/plugins";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db(process.env.MONGODB_DATABASE);
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db),
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }
+    },
+    session: {
+        expiresIn: 7 * 24 * 60 * 60,
+    },
+    plugins: [
+        jwt()
+    ]
+});
