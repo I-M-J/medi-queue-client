92da2b133685b9f5c1f0bd5be9e6cb0d543ed672
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-05-20 03:58:28 +0600

    proxy setup done

---
+++ src/app/add-tutor/page.jsx
@@ -0,0 +1,5 @@
+export default function AddTutorPage() {
+    return (
+        <div>AddTutorPage</div>
+    )
+}
---
+++ src/app/my-tutors/page.jsx
@@ -0,0 +1,5 @@
+export default function MyTutorsPage() {
+    return (
+        <div>MyTutorsPage</div>
+    )
+}
